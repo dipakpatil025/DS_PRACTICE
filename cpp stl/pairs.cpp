bb46c1c4c6cec5cc76b7bc7d22a6824eb0010912
Dipak Personal <dipak.02501@gmail.com>
2021-07-09 11:56:49 +0530

    addnig pairs and nesting

--- cpp stl/pairs.cpp
+++ cpp stl/pairs.cpp
@@ -11,13 +11,26 @@ long int gcd(long a, long b){return (b == 0) ? a : gcd(b,a%b);}
 int main()
 {
 
-    pair<int,int> p;
+    // pair<int,int> p;
     // p = make_pair(5,6);
-    p = {34,96};
+    // p = {34,96};
 
-    pair<int,int> &p1 = p;
-    p.first = 55;
-    cout<<p1.first<<" "<<p1.second<<endl;
+
+// coppy pair
+    // pair<int,int> &p1 = p;
+    // p.first = 55;
+    // cout<<p1.first<<" "<<p1.second<<endl;
+    
+
+    pair<int,int> p[3];
+    p[0] = make_pair(1,2);  
+    p[1] = make_pair(3,4);  
+    p[2] = make_pair(5,6);  
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<p[i].first<<" "<<p[i].second<<endl;
+    }
     
     return 0;
 }
