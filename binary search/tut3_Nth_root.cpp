2fb7a3283b3289859b23564ab8da9aee60bcd1ff
Dipak Personal <dipak.02501@gmail.com>
2021-08-10 22:19:08 +0530

    adding in binart serach

---
+++ binary search/tut3_Nth_root.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+double eps = 1e-6;
+double nTHroot(double n){
+    double l = 1;
+    double r = n;
+    while (r-l > eps)
+    {
+        double mid = (l + r)/2;
+        // if ((mid*mid) == n)
+        // {
+        //    return mid;
+        // }
+        
+        if ((mid*mid) < n)
+        {
+            l = mid;
+        }
+        else{
+            r = mid;
+        }
+        
+    }
+    // cout<<setprecision(20);
+    return l;
+}
+int main()
+{
+    double n;
+    cin>>n;
+    double ans = nTHroot(n);
+    cout<<ans<<endl;
+    
+    return 0;
+}
