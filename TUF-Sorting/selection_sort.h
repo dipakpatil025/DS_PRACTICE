af315d0db57d9801d457e18895d92197adcacb90
Dipak Patil <dipak.02501@gmail.com>
2023-01-31 00:09:50 +0530

    Add soting

---
+++ TUF-Sorting/selection_sort.h
@@ -0,0 +1,14 @@
+//
+// Created by Asus on 30-01-2023.
+//
+
+#ifndef DS_PRACTICE_SELECTION_SORT_H
+#define DS_PRACTICE_SELECTION_SORT_H
+
+
+class selection_sort {
+
+};
+
+
+#endif //DS_PRACTICE_SELECTION_SORT_H
